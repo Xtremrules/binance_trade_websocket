802fe35a2de3fe19ca2698c1e4acd83ef7ca88f9
“Chukwunyere <olelewechukwunyere@outlook.com>
2022-04-07 14:58:58 +0100

    Initial Project setup

---
+++ src/main/java/com/structure/binancetrade/BinanceTradeApplication.java
@@ -0,0 +1,13 @@
+package com.structure.binancetrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinanceTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinanceTradeApplication.class, args);
+	}
+
+}
