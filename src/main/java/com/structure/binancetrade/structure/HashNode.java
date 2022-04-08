f5bdbd69a45f9d545ef7494810f36ac96125523d
Chukwunyere Olelewe <olelewechukwunyere@outlook.com>
2022-04-08 03:52:28 +0100

    Implemented Hash algorithm

---
+++ src/main/java/com/structure/binancetrade/structure/HashNode.java
@@ -0,0 +1,12 @@
+package com.structure.binancetrade.structure;
+
+public class HashNode<K, V> {
+    K key;
+    V value;
+    HashNode<K, V> next;
+
+    public HashNode() {
+        this.key = key;
+        this.value = value;
+    }
+}
